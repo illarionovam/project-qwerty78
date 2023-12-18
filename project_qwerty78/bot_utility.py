d178558b8c32f6b64b8d035defcee79c2704100e
Mariia Illarionova <illarionovam@gmail.com>
2023-12-19 00:28:30 +0200

    moved constants to the constants.py

--- project_qwerty78/bot_utility.py
+++ project_qwerty78/bot_utility.py
@@ -12,10 +12,6 @@ def wrap_exception(func):
 
     return inner
 
-COMMAND_TO_COMMAND_FORMAT_MAP = {
-}
-
-
 def process_command(command, args, book):
     
     
@@ -23,13 +19,13 @@ def process_command(command, args, book):
 
 
 def check_possible_commands(command):
-    possible_commands = get_close_matches(command, COMMAND_TO_COMMAND_FORMAT_MAP.keys())
-    for key in COMMAND_TO_COMMAND_FORMAT_MAP.keys():
+    possible_commands = get_close_matches(command, constants.COMMAND_TO_COMMAND_FORMAT_MAP.keys())
+    for key in constants.COMMAND_TO_COMMAND_FORMAT_MAP.keys():
         if command in key or key in command:
             if key not in possible_commands:
                 possible_commands.append(key)
     
-    if len(possible_commands) == 0:
-        return constants.INVALID_COMMAND
-    
-    return f"{constants.INVALID_COMMAND} Maybe, you wanted to run one of these commands?\n\n" + "\n".join(COMMAND_TO_COMMAND_FORMAT_MAP[x] for x in possible_commands)
+    return (constants.INVALID_COMMAND 
+            if len(possible_commands) == 0 
+            else (f"{constants.INVALID_COMMAND}\nMaybe, you wanted to run one of these commands?\n\n" 
+                  + "\n".join(constants.COMMAND_TO_COMMAND_FORMAT_MAP[x] for x in possible_commands)))

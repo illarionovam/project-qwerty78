36ab022a3e9d4a2f4f51f12b7b15ecf611ebf29d
Mariia Illarionova <illarionovam@gmail.com>
2023-12-20 18:15:13 +0200

    added color schemes for all-contacts and all-notes
    added color for help command
    
    added easter eggs
    
    added remove confirmation to remove methods of the contact

---
+++ project_qwerty78/easter_eggs.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+class EasterEgg:
+    ENABLED = False
+
+    @staticmethod
+    def is_interesting_name(name):
+        if "slav" in name.lower():
+            print("Slava? Слава Україні! Героям слава! 💙💛") 
+
+    @staticmethod      
+    def is_interesting_phone(phone):
+        if "666" in phone:
+            print("Via Hell incorporated?")
+
+    @staticmethod
+    def is_interesting_birthday(birthday):
+        if birthday.startswith("29.02."):
+            print(
+                "Tough luck, kid. " 
+                + "Birthday is only once in 4 years? " 
+                + "No worries, we'll set it on 28.02 in case the year is not leap 😉")
+        elif datetime.strptime(birthday, "%d.%m.%Y").date().year < 1900:
+            print("Oh my... Is that dust from you?")

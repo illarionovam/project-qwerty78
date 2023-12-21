7fd587172b92411349480650de67b76c2e7a8831
Mariia Illarionova <illarionovam@gmail.com>
2023-12-21 21:47:51 +0200

    updated readme

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='project_qwerty78',
-      version='0.0.0',
+      version='0.1.0',
       description='Personal assistant bot is a console application that helps you to maintain your contacts and '
                   'notes. You can add phones, birthday, email and address to the contacts. You can add tags to the '
                   'notes. It saves your data locally on your machine.',

7a76f71f3919d875bef46360dcfb66d20fa0a237
Mariia Illarionova <illarionovam@gmail.com>
2023-12-22 10:46:02 +0200

    updated setup description

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='project_qwerty78',
-      version='0.1.1',
-      description='Personal assistant bot is a console application that helps you to maintain your contacts and '
-                  'notes. You can add phones, birthday, email and address to the contacts. You can add tags to the '
-                  'notes. It saves your data locally on your machine.',
+      version='0.1.2',
+      description='Personal assistant bot is a console application that helps you to maintain your contacts and notes. ' 
+                  'You can add phones, birthday, email and address to the contacts, edit contacts and remove them. '
+                  'You can add title and tags to the notes, edit notes and remove them. You can sort your notes by the date added and tags. '
+                  'It saves your data locally on your machine.',
       url='https://github.com/illarionovam/project-qwerty78',
       author='group-13',
       author_email='illarionovam@gmail.com',
